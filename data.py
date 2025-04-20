a9212189266672af0443e9f70d4575f3d0f7a32c
Kubasov Alexei <73648907+petushoque@users.noreply.github.com>
2025-04-20 07:32:18 +0700

    Initial commit

---
+++ data.py
@@ -0,0 +1,379 @@
+cocktails = [
+    {
+        "name": "Bloody Mary",
+        "ingredients": [
+            {"name": "Vodka", "amount": "45 ml"},
+            {"name": "Tomato Juice", "amount": "90 ml"},
+            {"name": "Lemon Juice", "amount": "15 ml"},
+            {"name": "Worcestershire Sauce", "amount": "2 dashes"},
+            {"name": "Tabasco", "amount": "2 dashes"},
+            {"name": "Salt", "amount": "pinch"},
+            {"name": "Pepper", "amount": "pinch"}
+        ],
+        "instructions": "Stir all ingredients with ice and strain into glass with fresh ice.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Margarita",
+        "ingredients": [
+            {"name": "Tequila", "amount": "60 ml"},
+            {"name": "Triple Sec", "amount": "30 ml"},
+            {"name": "Lime Juice", "amount": "30 ml"}
+        ],
+        "instructions": "Shake with ice and strain into a salt-rimmed glass.",
+        "glass": "Cocktail glass"
+    },
+    {
+        "name": "Mojito",
+        "ingredients": [
+            {"name": "White Rum", "amount": "60 ml"},
+            {"name": "Lime Juice", "amount": "30 ml"},
+            {"name": "Sugar", "amount": "2 tsp"},
+            {"name": "Mint", "amount": "6 leaves"},
+            {"name": "Soda Water", "amount": "to top"}
+        ],
+        "instructions": "Muddle mint with sugar and lime juice, add rum and ice, top with soda.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Cosmopolitan",
+        "ingredients": [
+            {"name": "Vodka", "amount": "45 ml"},
+            {"name": "Triple Sec", "amount": "15 ml"},
+            {"name": "Cranberry Juice", "amount": "30 ml"},
+            {"name": "Lime Juice", "amount": "15 ml"}
+        ],
+        "instructions": "Shake with ice and strain into chilled cocktail glass.",
+        "glass": "Cocktail glass"
+    },
+    {
+        "name": "Daiquiri",
+        "ingredients": [
+            {"name": "White Rum", "amount": "60 ml"},
+            {"name": "Lime Juice", "amount": "30 ml"},
+            {"name": "Sugar Syrup", "amount": "15 ml"}
+        ],
+        "instructions": "Shake with ice and strain into cocktail glass.",
+        "glass": "Cocktail glass"
+    },
+    {
+        "name": "Old Fashioned",
+        "ingredients": [
+            {"name": "Bourbon", "amount": "60 ml"},
+            {"name": "Sugar", "amount": "1 cube"},
+            {"name": "Angostura Bitters", "amount": "2 dashes"},
+            {"name": "Water", "amount": "1 tsp"}
+        ],
+        "instructions": "Muddle sugar and bitters with water, add bourbon and ice, stir.",
+        "glass": "Old fashioned glass"
+    },
+    {
+        "name": "Whiskey Sour",
+        "ingredients": [
+            {"name": "Bourbon", "amount": "60 ml"},
+            {"name": "Lemon Juice", "amount": "30 ml"},
+            {"name": "Sugar Syrup", "amount": "15 ml"}
+        ],
+        "instructions": "Shake with ice and strain into glass.",
+        "glass": "Old fashioned glass"
+    },
+    {
+        "name": "Mai Tai",
+        "ingredients": [
+            {"name": "White Rum", "amount": "30 ml"},
+            {"name": "Dark Rum", "amount": "30 ml"},
+            {"name": "Orange Curacao", "amount": "15 ml"},
+            {"name": "Lime Juice", "amount": "30 ml"},
+            {"name": "Orgeat Syrup", "amount": "15 ml"}
+        ],
+        "instructions": "Shake all ingredients with ice and strain into glass filled with crushed ice.",
+        "glass": "Old fashioned glass"
+    },
+    {
+        "name": "Piña Colada",
+        "ingredients": [
+            {"name": "White Rum", "amount": "60 ml"},
+            {"name": "Coconut Cream", "amount": "30 ml"},
+            {"name": "Pineapple Juice", "amount": "90 ml"}
+        ],
+        "instructions": "Blend all ingredients with ice until smooth and pour into chilled glass.",
+        "glass": "Hurricane glass"
+    },
+    {
+        "name": "Negroni",
+        "ingredients": [
+            {"name": "Gin", "amount": "30 ml"},
+            {"name": "Sweet Vermouth", "amount": "30 ml"},
+            {"name": "Campari", "amount": "30 ml"}
+        ],
+        "instructions": "Stir with ice and strain into glass with ice.",
+        "glass": "Old fashioned glass"
+    },
+    {
+        "name": "Long Island Iced Tea",
+        "ingredients": [
+            {"name": "Vodka", "amount": "15 ml"},
+            {"name": "Tequila", "amount": "15 ml"},
+            {"name": "White Rum", "amount": "15 ml"},
+            {"name": "Triple Sec", "amount": "15 ml"},
+            {"name": "Gin", "amount": "15 ml"},
+            {"name": "Lemon Juice", "amount": "25 ml"},
+            {"name": "Sugar Syrup", "amount": "30 ml"},
+            {"name": "Cola", "amount": "to top"}
+        ],
+        "instructions": "Shake all except cola with ice and strain into glass. Top with cola.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Manhattan",
+        "ingredients": [
+            {"name": "Rye Whiskey", "amount": "60 ml"},
+            {"name": "Sweet Vermouth", "amount": "30 ml"},
+            {"name": "Angostura Bitters", "amount": "2 dashes"}
+        ],
+        "instructions": "Stir with ice and strain into chilled glass.",
+        "glass": "Cocktail glass"
+    },
+    {
+        "name": "Tequila Sunrise",
+        "ingredients": [
+            {"name": "Tequila", "amount": "45 ml"},
+            {"name": "Orange Juice", "amount": "90 ml"},
+            {"name": "Grenadine", "amount": "15 ml"}
+        ],
+        "instructions": "Pour tequila and juice over ice. Add grenadine slowly to create gradient.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Aperol Spritz",
+        "ingredients": [
+            {"name": "Aperol", "amount": "60 ml"},
+            {"name": "Prosecco", "amount": "90 ml"},
+            {"name": "Soda Water", "amount": "30 ml"}
+        ],
+        "instructions": "Build in glass over ice and stir gently.",
+        "glass": "Wine glass"
+    },
+    {
+        "name": "Caipirinha",
+        "ingredients": [
+            {"name": "Cachaça", "amount": "60 ml"},
+            {"name": "Lime", "amount": "1/2 (cut into pieces)"},
+            {"name": "Sugar", "amount": "2 tsp"}
+        ],
+        "instructions": "Muddle lime with sugar, add cachaça and ice, stir.",
+        "glass": "Old fashioned glass"
+    },
+    {
+        "name": "Gin Tonic",
+        "ingredients": [
+            {"name": "Gin", "amount": "50 ml"},
+            {"name": "Tonic Water", "amount": "to top"}
+        ],
+        "instructions": "Build in glass over ice and stir lightly.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "French 75",
+        "ingredients": [
+            {"name": "Gin", "amount": "30 ml"},
+            {"name": "Lemon Juice", "amount": "15 ml"},
+            {"name": "Sugar Syrup", "amount": "15 ml"},
+            {"name": "Champagne", "amount": "to top"}
+        ],
+        "instructions": "Shake first three with ice, strain into flute, top with champagne.",
+        "glass": "Champagne flute"
+    },
+    {
+        "name": "Tom Collins",
+        "ingredients": [
+            {"name": "Gin", "amount": "60 ml"},
+            {"name": "Lemon Juice", "amount": "30 ml"},
+            {"name": "Sugar Syrup", "amount": "15 ml"},
+            {"name": "Soda Water", "amount": "to top"}
+        ],
+        "instructions": "Shake gin, lemon and syrup with ice. Strain into glass and top with soda.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Amaretto Sour",
+        "ingredients": [
+            {"name": "Amaretto", "amount": "60 ml"},
+            {"name": "Lemon Juice", "amount": "30 ml"},
+            {"name": "Sugar Syrup", "amount": "15 ml"}
+        ],
+        "instructions": "Shake with ice and strain into glass.",
+        "glass": "Old fashioned glass"
+    },
+    {
+        "name": "Espresso Martini",
+        "ingredients": [
+            {"name": "Vodka", "amount": "50 ml"},
+            {"name": "Coffee Liqueur", "amount": "25 ml"},
+            {"name": "Fresh Espresso", "amount": "25 ml"},
+            {"name": "Sugar Syrup", "amount": "10 ml"}
+        ],
+        "instructions": "Shake vigorously with ice and strain into chilled glass.",
+        "glass": "Cocktail glass"
+    },
+    {
+        "name": "Mint Julep",
+        "ingredients": [
+            {"name": "Bourbon", "amount": "60 ml"},
+            {"name": "Mint", "amount": "8 leaves"},
+            {"name": "Sugar", "amount": "1 tsp"},
+            {"name": "Water", "amount": "1 tsp"}
+        ],
+        "instructions": "Muddle mint with sugar and water, fill glass with crushed ice, add bourbon.",
+        "glass": "Julep cup"
+    },
+    {
+        "name": "Sidecar",
+        "ingredients": [
+            {"name": "Cognac", "amount": "50 ml"},
+            {"name": "Triple Sec", "amount": "20 ml"},
+            {"name": "Lemon Juice", "amount": "20 ml"}
+        ],
+        "instructions": "Shake with ice and strain into sugar-rimmed glass.",
+        "glass": "Cocktail glass"
+    },
+    {
+        "name": "Rum Punch",
+        "ingredients": [
+            {"name": "Dark Rum", "amount": "45 ml"},
+            {"name": "Orange Juice", "amount": "30 ml"},
+            {"name": "Pineapple Juice", "amount": "30 ml"},
+            {"name": "Lime Juice", "amount": "15 ml"},
+            {"name": "Grenadine", "amount": "15 ml"}
+        ],
+        "instructions": "Shake all ingredients with ice and strain into glass.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Cuba Libre",
+        "ingredients": [
+            {"name": "White Rum", "amount": "50 ml"},
+            {"name": "Lime Juice", "amount": "10 ml"},
+            {"name": "Cola", "amount": "to top"}
+        ],
+        "instructions": "Build in glass over ice, stir lightly.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Bellini",
+        "ingredients": [
+            {"name": "Peach Puree", "amount": "30 ml"},
+            {"name": "Prosecco", "amount": "90 ml"}
+        ],
+        "instructions": "Pour puree into flute, top with Prosecco gently.",
+        "glass": "Champagne flute"
+    },
+    {
+        "name": "Black Russian",
+        "ingredients": [
+            {"name": "Vodka", "amount": "50 ml"},
+            {"name": "Coffee Liqueur", "amount": "25 ml"}
+        ],
+        "instructions": "Build in glass over ice and stir.",
+        "glass": "Old fashioned glass"
+    },
+    {
+        "name": "White Russian",
+        "ingredients": [
+            {"name": "Vodka", "amount": "50 ml"},
+            {"name": "Coffee Liqueur", "amount": "25 ml"},
+            {"name": "Cream", "amount": "25 ml"}
+        ],
+        "instructions": "Build in glass over ice, stir gently.",
+        "glass": "Old fashioned glass"
+    },
+    {
+        "name": "Sex on the Beach",
+        "ingredients": [
+            {"name": "Vodka", "amount": "40 ml"},
+            {"name": "Peach Schnapps", "amount": "20 ml"},
+            {"name": "Orange Juice", "amount": "40 ml"},
+            {"name": "Cranberry Juice", "amount": "40 ml"}
+        ],
+        "instructions": "Shake with ice and strain into glass.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Gin Fizz",
+        "ingredients": [
+            {"name": "Gin", "amount": "45 ml"},
+            {"name": "Lemon Juice", "amount": "30 ml"},
+            {"name": "Sugar Syrup", "amount": "10 ml"},
+            {"name": "Soda Water", "amount": "to top"}
+        ],
+        "instructions": "Shake first three with ice, strain into glass and top with soda.",
+        "glass": "Highball glass"
+    },
+    {
+        "name": "Irish Coffee",
+        "ingredients": [
+            {"name": "Irish Whiskey", "amount": "45 ml"},
+            {"name": "Hot Coffee", "amount": "90 ml"},
+            {"name": "Brown Sugar", "amount": "1 tsp"},
+            {"name": "Heavy Cream", "amount": "30 ml"}
+        ],
+        "instructions": "Stir whiskey, coffee and sugar. Float cream on top.",
+        "glass": "Irish coffee mug"
+    }
+]
+
+ingredient_catalog = {
+    "Vodka": "Alcohol",
+    "Tequila": "Alcohol",
+    "White Rum": "Alcohol",
+    "Dark Rum": "Alcohol",
+    "Gin": "Alcohol",
+    "Triple Sec": "Liqueur",
+    "Orange Curacao": "Liqueur",
+    "Coffee Liqueur": "Liqueur",
+    "Peach Schnapps": "Liqueur",
+    "Amaretto": "Liqueur",
+    "Cognac": "Alcohol",
+    "Rye Whiskey": "Alcohol",
+    "Bourbon": "Alcohol",
+    "Irish Whiskey": "Alcohol",
+    "Cachaça": "Alcohol",
+    "Prosecco": "Sparkling Wine",
+    "Champagne": "Sparkling Wine",
+    "Sweet Vermouth": "Fortified Wine",
+    "Campari": "Liqueur",
+
+    "Lime Juice": "Juice",
+    "Lemon Juice": "Juice",
+    "Orange Juice": "Juice",
+    "Cranberry Juice": "Juice",
+    "Pineapple Juice": "Juice",
+    "Tomato Juice": "Juice",
+    "Peach Puree": "Juice",
+
+    "Sugar Syrup": "Sweetener",
+    "Sugar": "Sweetener",
+    "Brown Sugar": "Sweetener",
+    "Orgeat Syrup": "Sweetener",
+    "Grenadine": "Sweetener",
+
+    "Cream": "Dairy",
+    "Heavy Cream": "Dairy",
+    "Coconut Cream": "Dairy Alternative",
+
+    "Soda Water": "Mixer",
+    "Tonic Water": "Mixer",
+    "Cola": "Mixer",
+    "Hot Coffee": "Mixer",
+    "Fresh Espresso": "Mixer",
+    "Water": "Mixer",
+
+    "Mint": "Herb",
+    "Salt": "Spice",
+    "Pepper": "Spice",
+    "Angostura Bitters": "Bitters",
+    "Worcestershire Sauce": "Condiment",
+    "Tabasco": "Condiment",
+    "Lime": "Fruit",
+    "Ice": "Other"
+}
---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Query, HTTPException
+from fastapi.responses import JSONResponse
+from typing import List
+from data import cocktails, ingredient_catalog
+
+app = FastAPI(title="Cocktail API")
+
+
+@app.get("/cocktail/{name}")
+def get_cocktail_by_name(name: str):
+    for cocktail in cocktails:
+        if cocktail["name"].lower() == name.lower():
+            return cocktail
+    raise HTTPException(status_code=404, detail="Cocktail not found")
+
+
+@app.get("/cocktails")
+def get_all_cocktails():
+    return [cocktail["name"] for cocktail in cocktails]
+
+
+@app.get("/ingredients")
+def get_all_ingredients():
+    print('test')
+    return ingredient_catalog
+
+
+@app.get("/cocktails/search")
+def search_cocktails_by_ingredients(ingredients: List[str] = Query(...)):
+    print('test!')
+    input_ingredients = [ing.lower() for ing in ingredients]
+    matches = []
+
+    for cocktail in cocktails:
+        cocktail_ingredients = [i["name"].lower() for i in cocktail["ingredients"]]
+        match_count = sum(1 for ing in input_ingredients if ing in cocktail_ingredients)
+
+        if match_count > 0:
+            matches.append({
+                "cocktail": cocktail,
+                "match_count": match_count,
+                "total_ingredients": len(cocktail_ingredients)
+            })
+
+    matches.sort(key=lambda x: (-x["match_count"], x["total_ingredients"]))
+    return JSONResponse(content=[m["cocktail"] for m in matches])
